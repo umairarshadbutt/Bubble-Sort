878c17777d2dc1ba0bb7c257f37a7f64e720dfe2
umairarshadbutt <umair6622@gmail.com>
2020-07-13 20:49:14 +0500

    linter issues are fixed

--- main.rb
+++ main.rb
@@ -1,29 +1,27 @@
-number_array = [2,100,2,887,7,88,3,22,333,33333,12111,33434,546576576,6788799,4564534]
+number_array = [2, 100, 2, 887, 7, 88, 3, 22, 333, 33_333, 12_111, 33_434, 546_576_576, 6_788_799, 4_564_534]
 def bubble_sort(array)
-     puts "Unsorted array : #{array}" 
-    (0..array.length - 1).each do 
-        (0..array.length - 2).each do |index|
-            if (array[index] <=> array[index+1]).positive?
-                array[index], array[index+1] =array[index+1], array[index]
-            end
-        end
+  puts "Unsorted array : #{array}"
+  (0..array.length - 1).each do
+    (0..array.length - 2).each do |index|
+      array[index], array[index + 1] = array[index + 1], array[index] if (array[index] <=> array[index + 1]).positive?
     end
-    puts "Sorted array : #{array}" 
+  end
+  puts "Sorted array : #{array}"
 end
 
 def bubble_sort_by(array)
-    puts "Unsorted array : #{array}"
-    (0..array.length - 1).each do
-      (0..array.length - 2).each do |index|
-        next unless yield(array[index], array[index + 1]).positive?
-        array[index], array[index + 1] = array[index + 1], array[index]
-      end
+  puts "Unsorted array : #{array}"
+  (0..array.length - 1).each do
+    (0..array.length - 2).each do |index|
+      next unless yield(array[index], array[index + 1]).positive?
+      array[index], array[index + 1] = array[index + 1], array[index]
     end
-    print "Sorted array : #{array}"
   end
-string_array=["hi","hello","hey"]
+  print "Sorted array : #{array}"
+end
+string_array = %w[hi hello hey]
 bubble_sort(number_array)
-result = bubble_sort_by(string_array) do |left,right|
-    left.length - right.length
- end
- result
+result = bubble_sort_by(string_array) do |left, right|
+  left.length - right.length
+end
+result
