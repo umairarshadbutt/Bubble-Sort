d0ff0782b38e02aa784aeca7a3eaa24580342405
umairarshadbutt <umair6622@gmail.com>
2020-07-13 21:04:36 +0500

    Add empty line after guard clause

--- main.rb
+++ main.rb
@@ -8,11 +8,13 @@ def bubble_sort(array)
   end
   puts "Sorted array : #{array}"
 end
+
 def bubble_sort_by(array)
   puts "Unsorted array : #{array}"
   (0..array.length - 1).each do
     (0..array.length - 2).each do |index|
       next unless yield(array[index], array[index + 1]).positive?
+
       array[index], array[index + 1] = array[index + 1], array[index]
     end
   end
